567e6617d6cea1d3c7bd54a399e63a782575dc72
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-05-21 17:39:53 +0900

    Initail commit

---
+++ lua/term-gf/config.lua
@@ -0,0 +1,20 @@
+local M = {}
+
+local config = {
+	keymaps = {
+		goto_file = "CR",
+	},
+}
+
+function M.set(user_conf)
+	if user_conf == nil then
+		return config
+	end
+	if type(user_conf) ~= "table" then
+		error("Configuration error")
+	end
+	config = vim.tbl_deep_extend("force", config, user_conf or {})
+	return config
+end
+
+return M
---
+++ lua/term-gf/init.lua
@@ -0,0 +1,37 @@
+local M = {}
+
+local c = require("config")
+
+function M.setup(user_config)
+	local keymaps = c.set(user_config).keymaps
+
+	local group_name = "term-gf"
+	vim.api.nvim_create_augroup(group_name, { clear = true })
+
+	vim.api.nvim_create_autocmd({ "TermOpen", "TermEnter" }, {
+		group = group_name,
+		pattern = "term://*",
+		callback = function()
+			vim.keymap.set("n", keymaps.goto_file, function()
+				local file = vim.fn.expand("<cfile>")
+				local word = vim.fn.expand("<cWORD>")
+				local f = vim.fn.findfile(file)
+				local num = string.match(word, ":(%d*)")
+				if vim.fn.filereadable(f) == 1 then
+					vim.cmd("wincmd p")
+					vim.cmd("e " .. f)
+					if num ~= nil then
+						vim.cmd(num)
+						local col = string.match(word, ":%d*:(%d*)")
+						if col ~= nil then
+							vim.cmd("normal! " .. col .. "|")
+						end
+					end
+				end
+			end, { noremap = true, silent = true, buffer = true })
+		end,
+		once = false,
+	})
+end
+
+return M
